5694599e0e5db5faf84696707930b19a9901b93d
PraveenKumaresan <pachaihome@PRAVEENs-MacBook-Pro.local>
2017-03-09 19:52:27 -0600

    Default Constructor

---
+++ src/main/java/com/kpk/demo/ConstructorFirst.java
@@ -0,0 +1,22 @@
+package com.kpk.demo;
+
+
+public class ConstructorFirst {
+
+    String testVariable;
+
+    public ConstructorFirst() {
+        System.out.println("this reference -->" +this.testVariable);
+    }
+
+    public ConstructorFirst(String testVariable) {
+        this.testVariable = testVariable;
+        System.out.println("this is one argument -->" +this.testVariable);
+    }
+
+    public static void main (String[] args)
+    {
+        new ConstructorFirst();
+        new ConstructorFirst("Test Constructor with one Argument");
+    }
+}
--- src/main/java/com/kpk/demo/JavaDemoCaseStudyOneApplication.java
+++ src/main/java/com/kpk/demo/JavaDemoCaseStudyOneApplication.java
@@ -9,66 +9,56 @@ import java.util.Scanner;
 @SpringBootApplication
 public class JavaDemoCaseStudyOneApplication {
 
-	public JavaDemoCaseStudyOneApplication() {
-	}
+    public JavaDemoCaseStudyOneApplication() {
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaDemoCaseStudyOneApplication.class, args);
-		System.out.println("Demo for simple Java App");
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDemoCaseStudyOneApplication.class, args);
+        System.out.println("Demo for simple Java App");
 
-		englishPremierClub();
+        englishPremierClub();
 
-		ArrayList<String> clublist = new ArrayList<String>();
+        ArrayList<String> clublist = new ArrayList<String>();
 
-			clublist.add(0, "Chelsea");
-			clublist.add(1, "Aresnal");
-			clublist.add(2, "Liverpool");
-			clublist.add(3, "Manchester City");
-			clublist.add(4, "Tottenham Hotspurs");
-			clublist.add(5, "Manchester United");
-			clublist.add(6, "Watford");
-			clublist.add(7, "West Brom");
-			clublist.add(8, "Southampton");
-			clublist.add(9, "Stoke City");
+        clublist.add(0, "Chelsea");
+        clublist.add(1, "Aresnal");
+        clublist.add(2, "Liverpool");
+        clublist.add(3, "Manchester City");
+        clublist.add(4, "Tottenham Hotspurs");
+        clublist.add(5, "Manchester United");
+        clublist.add(6, "Watford");
+        clublist.add(7, "West Brom");
+        clublist.add(8, "Southampton");
+        clublist.add(9, "Stoke City");
 
-			System.out.println("size of club list" +clublist.size());
+        System.out.println("size of club list" + clublist.size());
 
-		/*System.out.println("Enter your favorite club to favorite player name");
-		Scanner sc  = new Scanner(System.in);
-		String input = sc.nextLine();*/
-
-		String input = null;
-		String valuePlayer = getPlayerOfClubs(clublist, input);
+        String input = null;
+        String valuePlayer = getPlayerOfClubs(clublist, input);
 
 
+        System.out.println("value player across club" + valuePlayer);
 
-		System.out.println("value player across club" +valuePlayer);
+    }
 
-	}
+    // display of english soccer
+    static void englishPremierClub() {
+        System.out.println("English Premier Soccer Clubs");
+    }
 
-// display of english soccer
+    // display list of clubs
+    static String getPlayerOfClubs(ArrayList clublist, String input) {
 
-	  static void englishPremierClub()
-	  {
-		System.out.println("English Premier Soccer Clubs");
-	  }
+        System.out.println("English premier club starts...");
 
-// display list of clubs
+        for (int i = 0; i < clublist.size(); i++) {
+            System.out.printf("Club by standing positions#%s ClubName-%s \n", i,
+                    clublist.get(i).toString());
+        }
 
-	   static String getPlayerOfClubs(ArrayList clublist, String input)
-	     {
-
-			System.out.println("English premier club starts...");
-
-		      for (int i =0 ; i < clublist.size(); i++)
-		      {
-			       System.out.printf("Club by standing positions#%s ClubName-%s \n", i,
-						   clublist.get(i).toString());
-		       }
-
-			 System.out.println("user inputs" +input);
+        System.out.println("user inputs" + input);
 
 //		     return (clublist.get(Integer.valueOf(input).toString()));
-			 return "Bruyne";
-	    }
+        return "Bruyne";
+    }
 }
